1f4ecf68e4baa5fe09690e23bce699bc1d8bc275
Ayu Ashari Ramadhan <53911794+ayuramadhann@users.noreply.github.com>
2020-06-28 19:10:15 +0700

    folder public src, etc

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>Username</h2>
+        <div>
+          <div v-for="myuser in user" v-bind:key="myuser.id" class="user">
+            <profileitem v-bind:myuser="myuser" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import "bootstrap/dist/css/bootstrap.css";
+import profileitem from "./components/profileitem.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      user: [
+        {
+          id: 1,
+          username: "ayuramadhan",
+          status: false
+        },
+        {
+          id: 2,
+          username: "ramadhan",
+          status: false
+        }
+      ]
+    };
+  },
+  components: {
+    profileitem
+  }
+};
+</script>
+
+<style scoped>
+.item-completed {
+  color: aquamarines;
+}
+</style>
---
+++ src/components/profileitem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input class="form-check-input" type="checkbox" id="defaultCheck1" />
+    <label class="mt-1" v-bind:class="{ 'item-completed': myuser.status }">
+      {{
+      myuser.username
+      }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profileitem",
+  props: ["myuser"]
+};
+</script>
+<style scoped></style>
